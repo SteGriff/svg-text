4d0ae4fe7e01ecea5213024fb1f1ae9f5a98815a
Glitch (hello-express) <none>
2026-02-17 14:32:42 +0000

    Disco hosting

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ const makeSvg = text => {
 };
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
